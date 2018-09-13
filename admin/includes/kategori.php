9af0e8e1cc87a575770b0cdd98bc416ddf4015b5
vertexPlayer <xyberworm@yahoo.com>
2018-09-13 23:46:20 +0800

    new files updated

---
+++ admin/includes/kategori.php
@@ -0,0 +1,25 @@
+<?php 
+
+class Kategori extends Db_object {
+
+	protected static $db_tables = "categories";
+	protected static $db_table_fields = array('id', 'title');
+
+	public $id;
+	public $title;
+
+
+
+
+}
+
+
+
+
+
+
+
+
+
+
+ ?>
---
+++ includes/sidebar.php
@@ -0,0 +1,57 @@
+        
+
+            <!-- Blog Search Well -->
+                <div class="panel panel-default">
+                    <div class="panel-heading">
+                        <h4>Blog Search</h4>
+                    </div>
+                    <div class="panel-body">
+                        <form action="search.php" method="post">
+                        <div class="input-group">
+                        <input type="text" class="form-control" name="search">
+                        <span class="input-group-btn">
+                            <button class="btn btn-primary" type="submit" name="cari">
+                                <span class="glyphicon glyphicon-search"></span>
+                        </button>
+                        </span>
+                        </div>
+                        </form>
+                    <!-- /.
+                    </div>
+                    input-group -->
+                    </div>
+                </div>
+
+                <!-- Blog Categories Well -->
+        <div class="panel panel-default">
+            <div class="panel-heading">
+                <h4>Blog Categories</h4>
+            </div>
+            <div class="panel-body">
+                <div class="row">
+                        <div class="col-lg-6">
+                            <ul class="list-unstyled">
+                                <li><a href="#">Technology</a>
+                                </li>
+                                <li><a href="#">Nature</a>
+                                </li>
+                                <li><a href="#">Abstract</a>
+                                </li>
+                                <li><a href="#">Phone</a>
+                                </li>
+                            </ul>
+                        </div>
+                        <!-- /.col-lg-6 -->
+                        
+                        <!-- /.col-lg-6 -->
+                    </div>
+            </div>
+                    
+                    
+                    <!-- /.row -->
+                </div>
+
+                <!-- Side Widget Well -->
+                
+
+            </div>
